fd0fef72becfe680017514c1ab82ca1b384e85be
Nikita <ymnooi1998@yandex.ru>
2022-10-24 19:01:53 +0300

    Initial Commit

---
+++ WYOH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WYOH
+//
+//  Created by Никита Зюзин on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
