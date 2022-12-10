71a50017af1883d60fa5c114d86fa9d3ab775b8f
Nikita <ymnooi1998@yandex.ru>
2022-12-10 20:13:08 +0300

    commit1

--- WYOH/ViewController.swift
+++ WYOH/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print (121233)
+        print (121235345234533)
     }
 
 
